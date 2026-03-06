963017b1b2e8a02b092ce96e94edb0d5ed7ac77b
yagutrslbyl <yaguter-bpa202@code.edu.az>
2026-03-06 15:03:40 +0400

    First Commit

---
+++ ironschool/src/test/java/org/example/ironschool/IronschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ironschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
